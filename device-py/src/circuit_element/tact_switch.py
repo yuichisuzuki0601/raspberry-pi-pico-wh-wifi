833717c8c2ba81327432ccb0922b4e46dedf5172
yuichi <ys.development0601@gmail.com>
2024-07-18 03:21:38 +0900

    チャタリングが発生するのは押したボタンのみなのでボタン別にdebounse時間管理する

--- device-py/src/circuit_element/tact_switch.py
+++ device-py/src/circuit_element/tact_switch.py
@@ -3,14 +3,13 @@ from utime import ticks_ms
 
 from board.pin_layout import to_gpio_number
 
-lastInterruptTime = 0
-
 class TactSwitch:
 	OFF = 0
 	ON = 1
 	BOUNCE_TIME_MS = 1500#[ms]
 
 	def __init__(self, name, pinNumber):
+		self.lastInterruptTime = 0
 		self.name = name
 		self.pin = Pin(to_gpio_number(pinNumber), Pin.IN, Pin.PULL_UP)
 
@@ -20,11 +19,10 @@ class TactSwitch:
 	def on_click(self, callback):
 		def handler(_):
 			# debouncing
-			global lastInterruptTime
 			currentTime = ticks_ms()
-			print('bounce: ' + str(currentTime - lastInterruptTime))
-			if currentTime - lastInterruptTime > TactSwitch.BOUNCE_TIME_MS:
-				print(self.name + ' tact switch clicked.')
+			print(f'bounce: {self.name} {str(currentTime - self.lastInterruptTime)}')
+			if currentTime - self.lastInterruptTime > TactSwitch.BOUNCE_TIME_MS:
+				print(f'{self.name} tact switch clicked.')
 				callback()
-				lastInterruptTime = currentTime
+				self.lastInterruptTime = currentTime
 		self.pin.irq(trigger = Pin.IRQ_FALLING, handler = handler)
